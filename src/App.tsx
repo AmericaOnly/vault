2d6eff1ce91df62f12add00cd9b18f94ea8506a9
mobiitz <mobilenetzllc@gmail.com>
2026-09-05 22:35:22 -0400

    Remove token menu from vault navbar

--- src/App.tsx
+++ src/App.tsx
@@ -81,9 +81,7 @@ export default function App() {
   return (
     <>
       <Navbar
-        currentPath={pathname}
         onNavigate={navigate}
-        farms={farmList}
       />
       {activeFarm ? (
         <FarmProvider config={activeFarm}>
